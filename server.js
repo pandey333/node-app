454363d52664e8f5fe735ea4fe164ad4f0a8b992
pandey333 <72303289+pandey333@users.noreply.github.com>
2025-06-18 18:44:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@
 const http = require('http');
 
 const HOST = '0.0.0.0';
-const PORT = 80;
+const PORT = 8000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
