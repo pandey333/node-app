82c44aa61eca9f971c68a54f0c04385287512806
pandey333 <72303289+pandey333@users.noreply.github.com>
2025-06-18 18:19:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-'use strict';
+// 'use strict';
 
-const express = require('express');
+// const express = require('express');
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+// // Constants
+// const PORT = 8080;
+// const HOST = '0.0.0.0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World..............I AM FROM BACKEND');
+// // App
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('Hello World..............I AM FROM BACKEND');
+// });
+
+// app.listen(PORT, HOST);
+// console.log(`Running on http://${HOST}:${PORT}`);
+
+
+const http = require('http');
+
+const PORT = 80;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from Node.js on port 80!\n');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
