f02464ed4a74b89ddc1609f05d408cc3e95bb293
pandey333 <72303289+pandey333@users.noreply.github.com>
2025-06-21 12:59:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = 8000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Node.js on port 8000!\n');
+  res.end('Hello I am Rajat from backend running on port 8000!\n');
 });
 
 server.listen(PORT, HOST, () => {
